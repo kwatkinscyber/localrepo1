d7e0896d435a6d2ab525d3a964ddaad7630a8b66
Khafre Watkins <kwatkins@zero2cyber.com>
2019-05-16 19:20:53 -0500

    added somefile

---
+++ somefile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function shwap()
+{
+echo "input please (options are shmack, purplecaddy, tailspin)"
+
+read x
+if  [ $x = shmack ];
+then
+echo "face hurt"
+
+elif  [ $x = purplecaddy ];
+then
+echo "ridin clean"
+
+elif  [ $x = tailspin ];
+then
+echo "flyhigh"
+
+else
+
